71b15afd3dd23b632c7dc9da86b6c67e67ae65e6
mononoaware <sk8l1f3111@gmail.com>
2024-12-20 12:41:40 -0800

    .

---
+++ src/main/java/com/example/canvasreminderbot/CanvasreminderbotApplication.java
@@ -0,0 +1,13 @@
+package com.example.canvasreminderbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanvasreminderbotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CanvasreminderbotApplication.class, args);
+    }
+
+}
